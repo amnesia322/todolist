4cd4c10c83452a666a3523b078984c49bcb3153d
amnesia322 <dev.amnesia322@gmail.com>
2022-12-15 16:06:23 +0300

    refactoring

--- src/components/EditableSpan/EditableSpan.tsx
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -1,11 +1,9 @@
 import React, { ChangeEvent, useState } from 'react';
 import TextField from '@mui/material/TextField';
 
-
 type EditableSpanPropsType = {
     value: string
     onChange: (newValue: string) => void
-    disabled?: boolean
 }
 
 export const EditableSpan = React.memo(function (props: EditableSpanPropsType) {
