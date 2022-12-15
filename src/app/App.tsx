98a0a2414ac8d750754f43c96842bb56b75e8ead
amnesia322 <dev.amnesia322@gmail.com>
2022-12-15 17:36:44 +0300

    add Login.tsx and Routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,11 +12,14 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import {Menu} from '@mui/icons-material';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {Login} from "../features/Login/Login";
 
 
 function App() {
     const status = useAppSelector<RequestStatusType>((state) => state.app.status)
     return (
+        <BrowserRouter>
         <div className="App">
             <ErrorSnackbar/>
             <AppBar position="static">
@@ -32,9 +35,16 @@ function App() {
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
-                <TodolistsList/>
+                <Routes >
+                    <Route path='/' element={ <TodolistsList />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>} />
+                    <Route path='*' element={<Navigate to='/404/'/>} />
+                </Routes>
+
             </Container>
         </div>
+        </BrowserRouter>
     )
 }
 
